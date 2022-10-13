d8e4c3927d522b61b2d1c460199901d1fa6f300b
nathiyasubramani <thanvihasree.subramani@gmail.com>
2022-10-13 10:51:13 +0100

    2nd commit

--- JavaIntermediate/src/Dependency_Inversion/Test.java
+++ JavaIntermediate/src/Dependency_Inversion/Test.java
@@ -1,11 +1,16 @@
 package Dependency_Inversion;
 
-import java.
-
 public class Test {
 	public static void main(String[] args)
 	{
+		WeatherTracker W=new WeatherTracker();
+		Phone p=new Phone();
+		p.alertWeatherConditions(null);
+		Email E= new Email();
+		E.alertWeatherConditions(null);
 		
+		W.setConditions("rainy");
+		System.out.println(W.getConditions());
 	}
 
 }
